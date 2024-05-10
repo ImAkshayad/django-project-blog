dd0a727e7c6c321e87f0e07b789769f4a5d85d30
akshd <aksh.devika@gmail.com>
2024-05-10 18:15:41 +0100

    Posts objects reg

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' : 'Jane Doe',
-        'title' : 'Times of India',
-        'content' : 'First post content',
-        'date_posted' : 'April 10, 2024'
-    },
-    {
-        'author' : 'John Connor',
-        'title' : 'The Mint',
-        'content' : 'Second post content',
-        'date_posted' : 'April 12, 2024'
-    },
-    {
-        'author' : 'Michael Kentucky',
-        'title' : 'London Times',
-        'content' : 'Third post content',
-        'date_posted' : 'April 12, 2024'
-    }
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
